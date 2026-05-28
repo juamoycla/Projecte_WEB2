d8d086da715569fddc12f139a6c5b6689e8a3e94
Juan <juamoycla@alu.edu.gva.es>
2026-05-28 19:07:23 +0200

    vercel

--- api/contacto.php
+++ api/contacto.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="../contacto/style.css"/>
     <title>2&4wheels | Contacto</title>
 </head>
 <body>
